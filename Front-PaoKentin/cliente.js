268698df6f9f215a2e5cf8b114c4144637a903bc
camilads <camila.dsantossilva@gamil.com>
2023-07-27 00:23:33 -0300

    add verificação do idPao para cards não repetir

--- Front-PaoKentin/cliente.js
+++ Front-PaoKentin/cliente.js
@@ -1,6 +1,6 @@
-// cliente.js
 document.addEventListener("DOMContentLoaded", function () {
     const urlApi = "http://localhost:8080/fornada";
+    const paoIdsExibidos = []; 
 
     async function buscarUltimasFornadas() {
         try {
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
             const fornadas = await response.json();
             mostrarUltimasFornadas(fornadas);
         } catch (error) {
-            console.error(error);
+            throw error;
         }
     }
 
@@ -38,14 +38,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function mostrarUltimasFornadas(fornadas) {
         const ultimasFornadasDiv = document.getElementById("ultimasFornadas");
-        ultimasFornadasDiv.innerHTML = "";
+        //ultimasFornadasDiv.innerHTML = "";
 
         fornadas.forEach((fornada) => {
             const pao = fornada.pao;
+            const paoId = pao.id;
             const tempoPreparo = pao.tempoPreparo * 60 * 1000; 
             const horaFornada = new Date(fornada.horaInicio).getTime();
             const horaAtual = new Date().getTime();
             const tempoDecorrido = horaAtual - horaFornada;
+
+            if(!paoIdsExibidos.includes(paoId)){
 
             const card = document.createElement("div");
             card.classList.add("col-md-4");
@@ -53,14 +56,31 @@ document.addEventListener("DOMContentLoaded", function () {
                 <div class="card mt-3">
                     <div class="card-body">
                         <h5 class="card-title">${pao.tipoPao}</h5>
-                        <p class="card-text">${tempoDecorrido >= tempoPreparo ? 'Fornada pronta há ' + formatarTempo(tempoDecorrido - tempoPreparo) : 'Tempo restante: ' + formatarTempo(tempoPreparo - tempoDecorrido)}</p>
+                        <p class="card-text">${tempoDecorrido >= tempoPreparo ? 'Fornada pronta há ' + 
+                        formatarTempo(tempoDecorrido - tempoPreparo) : 'Tempo restante: ' + 
+                        formatarTempo(tempoPreparo - tempoDecorrido)}</p>
                     </div>
                 </div>
             `;
 
             ultimasFornadasDiv.appendChild(card);
-        });
-    }
+            paoIdsExibidos.push(paoId);
+
+            }else{
+
+                const paoCards = document.querySelectorAll(".card-title");
+                paoCards.forEach((paoCard) => {
+                    if(paoCard.textContent === pao.tipoPao){
+                        const cardText = paoCard.nextElementSibling;
+                        cardText.textContent = tempoDecorrido >= tempoPreparo
+                            ? "Fornada pronta há " + formatarTempo(tempoDecorrido - tempoPreparo)
+                            : "Tempo restante: " + formatarTempo(tempoPreparo - tempoDecorrido);
+                    } 
+            });
+        }
+    });
+
+}
 
     buscarUltimasFornadas();
 });
